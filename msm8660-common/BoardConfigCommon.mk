61bf0cd1aeb032321d2bf4e7571f20062a1c9502
evnit <evnithn@gmail.com>
2015-05-17 13:24:59 +0700

    update .....

--- msm8660-common/BoardConfigCommon.mk
+++ msm8660-common/BoardConfigCommon.mk
@@ -97,6 +97,7 @@ TARGET_USES_QCOM_BSP := true
 
 # RIL
 BOARD_RIL_CLASS := ../../../device/samsung/msm8660-common/ril
+#BOARD_RIL_CLASS := ../../../hardware/samsung/ril
 
 # SELinux
 include device/qcom/sepolicy/sepolicy.mk
